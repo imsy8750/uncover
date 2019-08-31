c70d213ade986be0a9df04d081cea009e541915a
Shubham Yadav <imsy8750@gmail.com>
2019-08-31 17:33:58 +0000

    initial deploy commit

---
+++ app.js
@@ -0,0 +1,114 @@
+var express               = require("express"),
+    mongoose              = require("mongoose"),
+    passport              = require("passport"),
+    bodyParser            = require("body-parser"),
+    User                  = require("./models/user"),
+    LocalStrategy         = require("passport-local"),
+    passportLocalMongoose = require("passport-local-mongoose")
+    
+//mongoose.connect("mongodb://localhost/auth_demo_app");
+mongoose.connect('mongodb+srv://shubham07:shubham07@@cluster0-1vgha.mongodb.net/test?retryWrites=true&w=majority', {
+	useNewUrlParser: true,
+	useCreateIndex: true
+}).then(() => {
+	console.log('Connected to DB!');
+}).catch(err => {
+	console.log('ERROR:', err.message);
+});
+var app = express();
+app.set('view engine', 'ejs');
+app.use(express.static("public"));
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(require("express-session")({
+    secret: "Rusty is the best and cutest dog in the world",
+    resave: false,
+    saveUninitialized: false
+}));
+app.use(express.static("public"));
+
+app.use(passport.initialize());
+app.use(passport.session());
+
+passport.use(new LocalStrategy(User.authenticate()));
+passport.serializeUser(User.serializeUser());
+passport.deserializeUser(User.deserializeUser());
+app.use(function(req, res, next){
+		res.locals.currentUser = req.user;
+		next();
+});
+
+//============
+// ROUTES
+//============
+
+app.get("/", function(req, res){
+    res.render("home",{currentUser: req.username});
+});
+app.get("/clubs", function(req, res){
+    res.render("clubs");
+});
+
+app.get("/broadcasting", function(req, res){
+    res.render("broadcasting");
+});
+
+app.get("/fixtures", function(req, res){
+    res.render("fixtures");
+});
+
+
+// Auth Routes
+
+//show sign up form
+app.get("/register", function(req, res){
+   res.render("register"); 
+});
+//handling user sign up
+app.post("/register", function(req, res){
+    var newUser = new User({username: req.body.username});
+    User.register(newUser, req.body.password, function(err, user){
+        if(err){
+            console.log(err);
+            return res.render("register");
+        }
+        passport.authenticate("local")(req, res, function(){
+           res.redirect("/"); 
+        });
+    });
+});
+
+// LOGIN ROUTES
+//render login form
+app.get("/login", function(req, res){
+   res.render("login"); 
+});
+//login logic
+//middleware
+app.post("/login", passport.authenticate("local", {
+    successRedirect: "/",
+    failureRedirect: "/login"
+}) ,function(req, res){
+});
+
+app.get("/logout", function(req, res){
+    req.logout();
+    res.redirect("/");
+});
+
+
+function isLoggedIn(req, res, next){
+    if(req.isAuthenticated()){
+        return next();
+    }
+    res.redirect("/login");
+}
+
+
+
+
+
+
+
+app.listen(3000, () => {
+	console.log('server listening on port 3000');
+});
